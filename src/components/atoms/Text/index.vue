65511abf62942bec9a5af8f756d46ccf0921824e
Fahmi Ramadhan <fahmi.ra@photon.in>
2022-10-17 11:37:49 +0700

    Initial Commit

---
+++ src/components/atoms/Text/index.vue
@@ -0,0 +1,26 @@
+<!-- Type like 'h1', 'h2', 'h3', or 'p' -->
+
+<script>
+import "./styles.vue";
+
+export default {
+  name: "AtomsText",
+  props: {
+    styles: Object,
+    type: String,
+  },
+  computed: {
+    customTag: function () {
+      const tag = this?.type ?? "p";
+      return tag;
+    },
+  },
+};
+</script>
+
+<!-- Ui Render -->
+<template>
+  <component id="container-text" :is="customTag" :style="styles">
+    <slot />
+  </component>
+</template>
---
+++ src/components/atoms/Text/styles.vue
@@ -0,0 +1,6 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<style>
+#container-text {
+  margin: 0;
+}
+</style>
